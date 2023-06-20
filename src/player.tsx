67a62da36629dc93aaf3adb5d19c139ce71ddce5
Joshua Carter <carter.joshua.603@gmail.com>
2023-06-19 17:36:17 -0700

    Create <Player /> component that takes children with enter/exit props and converts them to liqvid elements and markers

---
+++ src/player.tsx
@@ -0,0 +1,153 @@
+import { Player as LiqvidPlayer, Script } from 'liqvid';
+import React, { ReactElement, ReactNode, useEffect, useMemo } from 'react';
+
+// resources
+import controls from '@env/controls';
+
+type Marker = {
+    id: string;
+    start: number;
+    end: number;
+};
+
+type ChildData = {
+    rawMarkers: Marker[];
+    updatedChildren: ReactNode[];
+};
+
+const processChildren = (children: ReactNode): ChildData => {
+    const extractMarkers = (node: ReactNode): ChildData => {
+        if (!React.isValidElement(node)) {
+            return { rawMarkers: [], updatedChildren: [node] };
+        }
+
+        const { props } = node as ReactElement;
+
+        if (!props) {
+            return { rawMarkers: [], updatedChildren: [node] };
+        }
+
+        const markers = [];
+        let newProps = { ...props };
+
+        if (props['data-exit']) {
+            const enter = parseInt(props['data-enter']) || 0;
+            const exit = parseInt(props['data-exit']) || 0;
+
+            markers.push(
+                { id: `timestamp/${enter}`, start: enter, end: enter },
+                { id: `timestamp/${exit}`, start: exit, end: exit }
+            );
+
+            // update props
+            newProps = {
+                ...props,
+                'data-from-first': `timestamp/${enter}`,
+                'data-from-last': `timestamp/${exit}`,
+            };
+        }
+
+        const { rawMarkers: childMarkers, updatedChildren: childNodes } =
+            loopChildren(props.children);
+
+        // if we have children
+        if (childNodes.length > 0) {
+            newProps.children = childNodes;
+        }
+
+        const updatedNode = React.cloneElement(node, {
+            ...newProps,
+        });
+
+        return {
+            rawMarkers: [...markers, ...childMarkers],
+            updatedChildren: [updatedNode],
+        };
+    };
+
+    const loopChildren = (children: ReactNode): ChildData => {
+        const rawMarkers = [] as Marker[];
+        const updatedChildren = [] as ReactNode[];
+
+        React.Children.toArray(children).forEach(node => {
+            const { rawMarkers: childMarkers, updatedChildren: childNodes } =
+                extractMarkers(node);
+            rawMarkers.push(...childMarkers);
+            updatedChildren.push(...childNodes);
+        });
+
+        return {
+            rawMarkers,
+            updatedChildren,
+        };
+    };
+
+    return loopChildren(children);
+};
+
+type PlayerProps = {
+    children: ReactNode;
+    duration: number;
+    onScriptChange?: (script: Script) => void;
+};
+
+export const Player = ({ children, duration, onScriptChange }: PlayerProps) => {
+    // extract marker info from our children and add marker props to them
+    const { rawMarkers, updatedChildren } = processChildren(children);
+
+    // convert raw markers to a full marker list
+    const markers = useMemo(() => {
+        const markers = rawMarkers
+            .sort((a, b) => a.start - b.start)
+            .reduce(
+                (collection, current) => {
+                    const { id, start } = current;
+                    const last = collection.slice(-1)[0];
+
+                    // if this is a duplicate
+                    if (last.id === id) {
+                        // ignore it
+                        return collection;
+                    }
+
+                    // else, add it along with a segment
+                    return [
+                        ...collection,
+                        {
+                            id: `filler/${last.end}/${start}`,
+                            start: last.end,
+                            end: start,
+                        },
+                        current,
+                    ];
+                },
+                [{ id: 'timestamp/0', start: 0, end: 0 }]
+            );
+        markers.push({
+            id: 'duration',
+            start: markers.slice(-1)[0].end,
+            end: duration,
+        });
+        return markers.map(
+            m => [m.id, m.start, m.end] as [string, number, number]
+        );
+    }, [JSON.stringify(rawMarkers)]);
+
+    const script = useMemo(
+        () => new Script(markers),
+        [JSON.stringify(markers)]
+    );
+
+    // handle a possible script change
+    useEffect(() => {
+        onScriptChange?.(script);
+    }, [script]);
+
+    return (
+        <LiqvidPlayer controls={controls} script={script}>
+            {updatedChildren}
+        </LiqvidPlayer>
+    );
+};
+
+export default Player;
